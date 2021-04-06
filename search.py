184813f5bf8a92212d864bc7a5508b2a71c0cdde
Duc Cao <td.cao@jellysmack.com>
2021-04-06 10:18:14 +0200

    :sparkles: Utility function for NearestNeighbors

---
+++ search.py
@@ -0,0 +1,46 @@
+from cuml.neighbors import NearestNeighbors
+
+
+def nearest_neighbors(test, embeddings, distance_threshold):
+    KNN = 50
+    if len(test) == 3:
+        KNN = 2
+    model = NearestNeighbors(n_neighbors=KNN, metric="cosine")
+    model.fit(embeddings)
+
+    preds = []
+
+    CHUNK = 1024 * 4
+
+    print("Finding similar products...")
+    CTS = len(embeddings) // CHUNK
+    if len(embeddings) % CHUNK != 0:
+        CTS += 1
+
+    for j in range(CTS):
+
+        a = j * CHUNK
+        b = (j + 1) * CHUNK
+        b = min(b, len(embeddings))
+        print("chunk", a, "to", b)
+        distances, indices = model.kneighbors(
+            embeddings[
+                a:b,
+            ]
+        )
+
+        for k in range(b - a):
+            IDX = np.where(
+                distances[
+                    k,
+                ]
+                <= distance_threshold
+            )[0]
+            IDS = indices[k, IDX]
+            o = test.iloc[IDS].posting_id.values
+            preds.append(o)
+
+    del model, distances, indices, embeddings
+    _ = gc.collect()
+
+    return preds
