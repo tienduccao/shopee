320874a32ec80dc9bc6235ab29716c14d7a047a4
Duc Cao <td.cao@jellysmack.com>
2021-04-06 12:27:57 +0200

    :sparkles: Upload utility scripts to Kaggle

---
+++ upload_utility_scripts.sh
@@ -0,0 +1,6 @@
+# To upload 3 modules at once
+kaggle kernels push -p kaggle_scripts_config/text/
+sleep 9
+kaggle kernels push -p kaggle_scripts_config/image/
+sleep 9
+kaggle kernels push -p kaggle_scripts_config/search/
