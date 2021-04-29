cc79fb21c69445b07f66cbe97ddc1682dfaf7562
Thomas Michel <t.michel@jellysmack.com>
2021-04-29 08:24:01 +0100

    :shirt: Apply linter changes to image.data.augmentations and small refactor of images.data.image_inference_dataset

--- shopee/image/data/image_inference_dataset.py
+++ shopee/image/data/image_inference_dataset.py
@@ -15,35 +15,35 @@ class ImageInferenceDataset(Dataset):
 
     Args:
         data: shopee dataframe (test/train)
-        images_root_dir: path to directory containing images
+        images_path: path to directory containing images
         size: size for resizing images
 
     Attributes:
         data:
-        images_root_dir:
+        images_path:
         augmentations: albumentations augmentations for inference obtained
             from get_inference_augmentations
     """
-    
+
     def __init__(
-        self, 
-        data: pd.DataFrame, 
-        images_root_dir: str, 
+        self,
+        data: pd.DataFrame,
+        images_path: str,
         size: Tuple[int, int] = DEFAULT_SIZE,
     ):
+        super().__init__()
         self.images = list(data.image)
         self.augmentations = get_inference_augmentations(size)
-        self.images_root_dir = images_root_dir
-        
+        self.images_path = images_path
+
     def __len__(self) -> int:
         """Length of the Dataset"""
         return len(self.images)
-    
+
     def __getitem__(self, index: int) -> torch.Tensor:
         """Return image corresponding to row index"""
-        path = os.path.join(self.images_root_dir, self.images[index])
+        path = os.path.join(self.images_path, self.images[index])
         image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
         image = self.augmentations(image=image)["image"]
-            
-        return image
 
+        return image
