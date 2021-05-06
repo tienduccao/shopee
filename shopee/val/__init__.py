1bf97830ee945da9a137049fb7382db3776a4924
Thomas Michel <t.michel@jellysmack.com>
2021-05-06 09:10:45 +0100

    :sparkles: Add compute_knn in val module

--- shopee/val/__init__.py
+++ shopee/val/__init__.py
@@ -1,1 +1,2 @@
 from .compute_f1_score import compute_f1_score
+from .compute_knn import compute_knn
---
+++ shopee/val/compute_knn.py
@@ -0,0 +1,41 @@
+from typing import Tuple
+
+from cuml.neighbors import NearestNeighbors
+import numpy as np
+
+
+def compute_knn(
+    embeddings: np.ndarray, n_neighbors: int = 50
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Compute KNN from embeddings by using chunks of rows and by using cosine
+    distance.
+
+    Args:
+        embeddings: array of shape (N, C) where N is number of samples and C
+            embeddings dimension
+        n_neighbors: number of neighbors to compute
+
+    Returns:
+        knn_scores: array of shape (N, n_neighbors) containing the KNN
+            distances (in increasing order, sample included)
+        knn_indices: array of shape (N, n_neighbors) containing the indices of
+            the KNN
+    """
+
+    model = NearestNeighbors(n_neighbors=n_neighbors, metric="cosine")
+    model.fit(embeddings)
+
+    n_embeddings = len(embeddings)
+
+    knn_scores = np.zeros((n_embeddings, n_neighbors), dtype=np.float32)
+    knn_indices = np.zeros((n_embeddings, n_neighbors), dtype=np.int32)
+
+    chunks = 1024
+    for i in range(0, n_embeddings, chunks):
+        (
+            knn_scores[i : i + chunks],
+            knn_indices[i : i + chunks],
+        ) = model.kneighbors(embeddings[i : i + chunks])
+
+    return knn_scores, knn_indices
